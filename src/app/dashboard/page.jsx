1f74aefc1bbf6b10a4650c2d1c09dff3a274aff8
Shi Pengcheng <133251776+itSpc991@users.noreply.github.com>
2023-11-07 22:35:42 +0800

    完善

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,34 +1,30 @@
-"use client"
-
-import React, { useEffect, useState } from 'react'
-import styles from './page.module.css'
-import useSWR from 'swr';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
-  const session = useSession()
+  const session = useSession();
 
-  const router = useRouter()
+  const router = useRouter();
 
-  // 使用SWR Hook 获取数据
+  //NEW WAY TO FETCH DATA
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, mutate, error, isLoading } = useSWR(
     `/api/posts?username=${session?.data?.user.name}`,
     fetcher
-    );
+  );
 
-    console.log(data)
-
-  if (session.status === 'loading') {
-    return <p>Loading...</p>
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
   }
 
-  if (session.status === 'unauthenticated') {
-    router.push('/dashboard/login')
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
   }
 
   const handleSubmit = async (e) => {
@@ -39,61 +35,74 @@ const Dashboard = () => {
     const content = e.target[3].value;
 
     try {
-      await fetch('/api/posts', {
-        method: 'POST',
+      await fetch("/api/posts", {
+        method: "POST",
         body: JSON.stringify({
-          title, 
-          desc, 
-          img, 
-          content, 
-          username: session.data.user.name
-          }),
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
       });
       mutate();
       e.target.reset();
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   const handleDelete = async (id) => {
     try {
       await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       mutate();
     } catch (err) {
       console.log(err);
     }
+  };
+
+  if (session.status === "authenticated") {
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading"
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={100} />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
+};
 
-    if (session.status === 'authenticated') {
-  return( 
-    <div className={styles.container}>
-      <div className={styles.posts}>
-        {isLoading ? "loading" : data.map((post) => (
-          <div className={styles.post} key={post._id}>
-            <div className={styles.imgContainer}>
-              <Image src={post.img} alt="" width={200} height={100} />
-            </div>
-            <h2 className={styles.postTitle}>{post.title}</h2>
-            <span className={styles.delete} onClick={() => handleDelete(post._id)}>X</span>
-          </div>
-        ))}
-      </div>
-      <div className={styles.new} onSubmit={handleSubmit}>
-        <h1>Add New Post</h1>
-        <input type="text" placeholder='Title' className={styles.input} />
-        <input type="text" placeholder='Desc' className={styles.input} />
-        <input type="text" placeholder='Image' className={styles.input} />
-        <textarea placeholder='Content' className={styles.textArea} cols="30" rows="10" />
-        <button className={styles.button}>Send</button>
-      </div>
-    </div>
-)}}
-export default Dashboard
-
-
+export default Dashboard;
 
 // 使用Effect Hook 获取数据
 
@@ -105,7 +114,7 @@ export default Dashboard
 //     const getData = async () => {
 //       setIsLoading(true);
 //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//       // 10秒后重新获取数据 ,如果数据需要改变，选择此种方式 
+//       // 10秒后重新获取数据 ,如果数据需要改变，选择此种方式
 //       // next: { revalidate: 10},
 //       // 不需要缓存数据
 //       cache: 'no-cache',
@@ -114,7 +123,7 @@ export default Dashboard
 //     if (!res.ok) {
 //       setErr(true);
 //     }
-    
+
 //     const data = await res.json();
 //     setData(data);
 //     setIsLoading(false);
